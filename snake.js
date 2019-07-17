7e82531c11a408364c7d29d397686af049a8e4ec
AERIANDI\jonathanm <jonathan.madelaine@aeriandi.com>
2019-07-17 10:23:49 +0100

    Improve food creation

--- snake.js
+++ snake.js
@@ -1,38 +1,59 @@
+// Variables
 const canvas = document.getElementById('game-canvas')
 const context = canvas.getContext('2d')
 
+const colors = {
+    background: 'black',
+    snake: 'orange',
+    food: 'lime',
+}
+
 const cellSize = {
-    width: 64,
-    height: 64,
+    width: 32,
+    height: 32,
 }
 
 const gridSize = {
-    width: 10,
-    height: 10,
+    width: 24,
+    height: 16,
 }
 
-const getPositionOnGrid = (cellX, cellY) => {
-    if (cellX < 0) {
-        cellX = gridSize.width - 1
+const maxFoodCount = 5
+
+let score = 0
+let snakeDirection = 'up'
+let canChangeDirection = true
+let hasEatenFoodOnLastMove = false
+let gameOver = false
+let snake = []
+let food = []
+
+// Set the canvas size
+canvas.width = gridSize.width * cellSize.width
+canvas.height = gridSize.height * cellSize.height
+
+const getGridPosition = (x, y) => {
+    if (x < 0) {
+        x = gridSize.width - 1
     }
 
-    if (cellY < 0) {
-        cellY = gridSize.height - 1
+    if (y < 0) {
+        y = gridSize.height - 1
     }
 
-    cellX = cellX % gridSize.width
-    cellY = cellY % gridSize.height
+    x %= gridSize.width
+    y %= gridSize.height
 
     return {
-        position: {
-            x: cellX,
-            y: cellY,
+        pos: {
+            x,
+            y,
         },
     }
 }
 
-const getRandomNumber = max => {
-    return Math.floor(Math.random() * max)
+const getRandomInteger = exclusiveMax => {
+    return Math.floor(Math.random() * exclusiveMax)
 }
 
 const getRandomGridCell = cellsToExclude => {
@@ -41,45 +62,23 @@ const getRandomGridCell = cellsToExclude => {
     while (
         randomCell === null ||
         cellsToExclude.some(
-            c =>
-                c.position.x === randomCell.position.x &&
-                c.position.y === randomCell.position.y
+            c => c.pos.x === randomCell.pos.x && c.pos.y === randomCell.pos.y
         )
     ) {
-        randomCell = getPositionOnGrid(
-            getRandomNumber(gridSize.width),
-            getRandomNumber(gridSize.height)
+        randomCell = getGridPosition(
+            getRandomInteger(gridSize.width),
+            getRandomInteger(gridSize.height)
         )
     }
     return randomCell
 }
 
-const groundColor = 'black'
-const snakeColor = 'orange'
-const foodColor = 'lime'
-
-let snakeBodyParts = []
-snakeBodyParts[0] = getPositionOnGrid(gridSize.width / 2, gridSize.height / 2)
-snakeBodyParts[1] = getPositionOnGrid(
-    gridSize.width / 2,
-    gridSize.height / 2 + 1
-)
-snakeBodyParts[2] = getPositionOnGrid(
-    gridSize.width / 2,
-    gridSize.height / 2 + 2
-)
-
-let foodItems = []
-foodItems[0] = getRandomGridCell(snakeBodyParts)
-
-let score = 0
-
 // Control the snake
-
-// snake starts the game moving upwards
-let snakeDirection = 'up'
-
 const setSnakeDirection = event => {
+    if (!canChangeDirection) {
+        return
+    }
+
     const keyCode = event.keyCode
     if (keyCode === 37 && snakeDirection !== 'right') {
         snakeDirection = 'left'
@@ -90,54 +89,29 @@ const setSnakeDirection = event => {
     } else if (keyCode === 40 && snakeDirection !== 'up') {
         snakeDirection = 'down'
     }
+
+    canChangeDirection = false
 }
 
-document.addEventListener('keydown', setSnakeDirection)
-
 // Draw to the canvas
+const paintRect = (x, y, w, h, color, borderWidth, borderColor) => {
+    context.fillStyle = color
+    context.fillRect(x, y, w, h)
 
-const drawToCanvas = () => {
-    for (let i = 0; i < gridSize.width; i++) {
-        for (let j = 0; j < gridSize.height; j++) {
-            let fillColor = groundColor
-
-            if (
-                snakeBodyParts.some(
-                    s => s.position.x === i && s.position.y === j
-                )
-            ) {
-                fillColor = snakeColor
-            } else if (
-                foodItems.some(f => f.position.x === i && f.position.y === j)
-            ) {
-                fillColor = foodColor
-            }
-
-            context.fillStyle = fillColor
-            context.fillRect(
-                cellSize.width * i,
-                cellSize.height * j,
-                cellSize.width,
-                cellSize.height
-            )
-        }
-    }
-
-    for (let i = 0; i < snakeBodyParts.length; ++i) {
-        context.lineWidth = '2'
-        context.strokeStyle = 'yellow'
+    if (borderWidth && borderColor) {
+        context.lineWidth = borderWidth
+        context.strokeStyle = borderColor
         context.strokeRect(
-            cellSize.width * snakeBodyParts[i].position.x + 1,
-            cellSize.height * snakeBodyParts[i].position.y + 1,
-            cellSize.width - 2,
-            cellSize.height - 2
+            x + Math.floor(borderWidth / 2),
+            y + Math.floor(borderWidth / 2),
+            w - Math.floor(borderWidth),
+            h - Math.floor(borderWidth)
         )
     }
+}
 
-    context.fillStyle = 'white'
-    context.font = '32px Consolas'
-    context.fillText(score, 12, 32)
-
+const drawToCanvas = () => {
+    // paint the game over message
     if (gameOver) {
         context.fillStyle = 'white'
         context.font = '48px Consolas'
@@ -146,17 +120,56 @@ const drawToCanvas = () => {
             cellSize.width * gridSize.width * 0.5 - 120,
             cellSize.height * gridSize.height * 0.5 - 20
         )
+        return
     }
+
+    // paint background first
+    context.fillStyle = colors.background
+    paintRect(
+        0,
+        0,
+        gridSize.width * cellSize.width,
+        gridSize.height * cellSize.height,
+        colors.background
+    )
+
+    // paint the food
+    for (let i = 0; i < food.length; i++) {
+        paintRect(
+            cellSize.width * food[i].pos.x,
+            cellSize.height * food[i].pos.y,
+            cellSize.width,
+            cellSize.height,
+            colors.food
+        )
+    }
+
+    // paint the snake
+    for (let i = 0; i < snake.length; i++) {
+        paintRect(
+            cellSize.width * snake[i].pos.x,
+            cellSize.height * snake[i].pos.y,
+            cellSize.width,
+            cellSize.height,
+            colors.snake,
+            2,
+            'white'
+        )
+    }
+
+    // paint the score
+    context.fillStyle = 'white'
+    context.font = '32px Consolas'
+    context.fillText(score, 12, 32)
 }
 
-canvas.width = gridSize.width * cellSize.width
-canvas.height = gridSize.height * cellSize.height
-
-let hasEatenFood = false
-
 const getNewSnakePosition = () => {
-    let headPositionX = snakeBodyParts[0].position.x
-    let headPositionY = snakeBodyParts[0].position.y
+    if (snake.length < 1) {
+        return
+    }
+
+    let headPositionX = snake[0].pos.x
+    let headPositionY = snake[0].pos.y
 
     if (snakeDirection === 'up') {
         --headPositionY
@@ -168,51 +181,58 @@ const getNewSnakePosition = () => {
         --headPositionX
     }
 
-    snakeBodyParts.unshift(getPositionOnGrid(headPositionX, headPositionY))
+    snake.unshift(getGridPosition(headPositionX, headPositionY))
 
     // remove last tail body part
-    if (!hasEatenFood) {
-        snakeBodyParts.pop()
+    if (!hasEatenFoodOnLastMove) {
+        snake.pop()
     }
 
-    hasEatenFood = false
+    hasEatenFoodOnLastMove = false
+    canChangeDirection = true
 }
 
 const tryEatingFood = () => {
-    const snakeHeadPosition = snakeBodyParts[0].position
+    if (snake.length < 1) {
+        return
+    }
+    const snakeHeadPosition = snake[0].pos
 
     if (
-        foodItems.some(
+        food.some(
             f =>
-                f.position.x === snakeHeadPosition.x &&
-                f.position.y === snakeHeadPosition.y
+                f.pos.x === snakeHeadPosition.x &&
+                f.pos.y === snakeHeadPosition.y
         )
     ) {
-        ++score
-        hasEatenFood = true
-        foodItems = foodItems.filter(
+        food = food.filter(
             f =>
-                f.position.x !== snakeHeadPosition.x &&
-                f.position.y !== snakeHeadPosition.y
+                !(
+                    f.pos.x === snakeHeadPosition.x &&
+                    f.pos.y === snakeHeadPosition.y
+                )
         )
+        ++score
+        hasEatenFoodOnLastMove = true
     }
 }
 
 const generateRandomFoodItem = () => {
-    if (foodItems.length === 0) {
-        foodItems.push(getRandomGridCell(snakeBodyParts))
-    }
+    if(food.length < maxFoodCount)
+    food.push(getRandomGridCell(snake))
+    setTimeout(() => {
+        console.log('new')
+        generateRandomFoodItem()
+    }, (getRandomInteger(10) + 1) * 1000)
 }
 
-let gameOver = false
-
 const checkForCollision = () => {
-    if (snakeBodyParts.length > 1) {
-        const snakeHeadPosition = snakeBodyParts[0].position
-        for (let i = 1; i < snakeBodyParts.length; i++) {
+    if (snake.length > 1) {
+        const snakeHeadPosition = snake[0].pos
+        for (let i = 1; i < snake.length; i++) {
             if (
-                snakeBodyParts[i].position.x === snakeHeadPosition.x &&
-                snakeBodyParts[i].position.y === snakeHeadPosition.y
+                snake[i].pos.x === snakeHeadPosition.x &&
+                snake[i].pos.y === snakeHeadPosition.y
             ) {
                 gameOver = true
             }
@@ -220,9 +240,9 @@ const checkForCollision = () => {
     }
 }
 
-const drawNextFrame = () => {
+const update = () => {
     if (gameOver) {
-        clearInterval(game)
+        clearInterval(gameLoop)
     }
     // First calculate new positon of snake
     getNewSnakePosition()
@@ -233,10 +253,18 @@ const drawNextFrame = () => {
     drawToCanvas()
 }
 
-let game = setInterval(() => {
-    drawNextFrame()
-}, 175)
+// Setup game
 
-setInterval(() => {
-    generateRandomFoodItem()
-}, getRandomNumber(8) * 1000)
+// Create the snake with three sections in the middle of the canvas
+snake[0] = getGridPosition(gridSize.width / 2, gridSize.height / 2)
+snake[1] = getGridPosition(gridSize.width / 2, gridSize.height / 2 + 1)
+snake[2] = getGridPosition(gridSize.width / 2, gridSize.height / 2 + 2)
+
+// add first piece of food
+generateRandomFoodItem()
+
+document.addEventListener('keydown', setSnakeDirection)
+
+let gameLoop = setInterval(update, 175)
+
+setTimeout(generateRandomFoodItem, (getRandomInteger(10) + 1) * 1000)
